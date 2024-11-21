79bdfc94259d5dbb54941266f103b5601759098a
Suvanga <83526481+Suvanga@users.noreply.github.com>
2024-11-20 19:31:42 -0500

    Time: 0 ms (100%), Space: 12.5 MB (25.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,18 +1,22 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        a = len(nums)-1
-        b = len(nums)-1
-        for i in range(a):
-            for j in range (b):
-                if i!=j+1:
-                    if target == nums[i] + nums[j+1]:
-                        return(i,j+1)
-            j= j+1 
-        i=i+1       
-                
+        numMap = {}
+        n= len(nums)
+
+        for i in range(n):
+            compliment = target-nums[i]
+            if compliment in numMap:
+                return [numMap[compliment], i]
+            numMap[nums[i]]= i 
+        return []
+            
         
+        # a = len(nums)-1
+        # b = len(nums)-1
+        # for i in range(a):
+        #     for j in range (b):
+        #         if i!=j+1:
+        #             if target == nums[i] + nums[j+1]:
+        #                 return(i,j+1)
+        #     j= j+1 
+        # i=i+1       
