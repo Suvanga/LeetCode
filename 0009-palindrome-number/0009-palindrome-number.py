d86cdbfeff4112ba6973c5854bfd221360b2f814
Suvanga <83526481+Suvanga@users.noreply.github.com>
2024-11-20 20:10:50 -0500

    Time: 11 ms (55.03%), Space: 11.5 MB (60.05%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -5,10 +5,11 @@ class Solution(object):
         :rtype: bool
         """
         reverse = (str(x)[::-1])
-        if reverse == str(x):
-            return True
-        else:
-            return False
+        return reverse==str(x)
+        # if reverse == str(x):
+        #     return True
+        # else:
+        #     return False
        
        #So how it works is [::-1] -1 is used to split it in reverse order and it starts at the end since it is not specified  [start:stop:step] since step is -1 so it moves in negative -1 stepa and when we use the start as a different number it starts at a different number for example :
        # 10,20,30 when [1:0:-1] is used we can simply reverse it using  20
