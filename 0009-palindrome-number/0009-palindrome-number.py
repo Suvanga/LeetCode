9d32e8577a7f289f57b83cb71a4b6976c2c5b553
Suvanga <83526481+Suvanga@users.noreply.github.com>
2024-11-20 20:06:31 -0500

    Time: 5 ms (83.14%), Space: 11.7 MB (27.31%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -11,3 +11,7 @@ class Solution(object):
         else:
             return False
        
+       #So how it works is [::-1] -1 is used to split it in reverse order and it starts at the end since it is not specified  [start:stop:step] since step is -1 so it moves in negative -1 stepa and when we use the start as a different number it starts at a different number for example :
+       # 10,20,30 when [1:0:-1] is used we can simply reverse it using  20
+       # 10,20,30,40 when [3:1:-1] is used we can simply reverse it using  40,30 as it stops before the stop index which is 1 
+       # 10,20,30 wh
