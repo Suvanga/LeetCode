9da4f25ae274a6214cd7312a549a77876b4ed1ef
Dhakal <sdhakal8@rockets.utoledo.edu>
2025-11-15 18:24:06 -0500

    Another one is also solved

---
+++ MEDIUM/Group_Anagrams/sol.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+from typing import List
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
+    
+
+groupAnagrams = Solution().groupAnagrams
+
+# Example usage:
+sol = Solution()
+if __name__ == "__main__": # we use this because the platform may run all code at import time
+    input_strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+    print(sol.groupAnagrams(input_strs))
